230aca91bfeb0047ff9a0a1a65a94d8807ea11f1
Tyrulien <69004669+Tyrulien@users.noreply.github.com>
2024-08-16 21:07:02 +0100

    Update
    Update to various files

--- products.sql
+++ products.sql
@@ -8,3 +8,8 @@ pdescription VARCHAR(500) NOT NULL,
 pname VARCHAR(30)NOT NULL,
 date TIMESTAMP
 );
+
+INSERT INTO products (price, category, pdescription, pname, DATE)
+VALUES (1500, 1, 'Delicious dark roast coffee', 'Dark Roast', NOW()),
+       (1200, 2, 'Creamy vanilla latte', 'Vanilla Latte', NOW()),
+       (1800, 1, 'Premium blend espresso', 'Espresso', NOW());
