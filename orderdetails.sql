9f104dba5ee72337baea6ef8a4811180b617f9d5
Tyrulien <69004669+Tyrulien@users.noreply.github.com>
2024-08-30 00:08:53 +0100

    Delete orderdetails.sql

--- orderdetails.sql
+++
@@ -1,9 +0,0 @@
-USE 	coffeeshop;
-CREATE TABLE orderdetails (
-orderdetailsId INT(11) AUTO_INCREMENT PRIMARY KEY,
-quantity INT (11) NOT NULL,
-price INT (11) NOT NULL,
-orderId INT (11) NOT NULL,
-productid INT (11) NOT NULL,
-date TIMESTAMP
-);
