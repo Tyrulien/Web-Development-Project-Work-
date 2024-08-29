e343cb2e48e02380b424c34ea87c42dedb745919
Tyrulien <69004669+Tyrulien@users.noreply.github.com>
2024-08-30 00:09:57 +0100

    Delete users.sql

--- users.sql
+++
@@ -1,9 +0,0 @@
-USE 	coffeeshop;
-CREATE TABLE users (
-userid INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50) NOT NULL,
-ordersnum VARCHAR(200) NOT NULL,
-date TIMESTAMP
-);
