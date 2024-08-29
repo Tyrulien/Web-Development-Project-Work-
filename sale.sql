47819f739ac7adfcd71751fd250c9dfaf0a1dca6
Tyrulien <69004669+Tyrulien@users.noreply.github.com>
2024-08-30 00:09:41 +0100

    Delete sale.sql

--- sale.sql
+++
@@ -1,8 +0,0 @@
-USE 	coffeeshop;
-CREATE TABLE sale (
-saleId INT(45) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-salePrice INT(45) NOT NULL,
-sellerNumber INT(45) NOT NULL,
-productid INT(45) NOT NULL,
-date TIMESTAMP
-);
